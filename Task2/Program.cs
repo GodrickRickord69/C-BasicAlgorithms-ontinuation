631267df7b9caba718aeb2c7b98a4251e7527bf3
Nikita <varashilov125@gmail.com>
2023-06-21 19:34:38 +0300

    Task2 прописал через Parse

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,18 +4,18 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
 Console.WriteLine("Введите координаты точки А: ");
-int ax = Convert.ToInt32(Console.ReadLine()!);
+int ax = int.Parse(Console.ReadLine()!);
 
-int ay = Convert.ToInt32(Console.ReadLine()!);
+int ay = int.Parse(Console.ReadLine()!);
 
-int az = Convert.ToInt32(Console.ReadLine()!);
+int az = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите координаты точки B: ");
-int bx = Convert.ToInt32(Console.ReadLine()!);
+int bx = int.Parse(Console.ReadLine()!);
 
-int by = Convert.ToInt32(Console.ReadLine()!);
+int by = int.Parse(Console.ReadLine()!);
 
-int bz = Convert.ToInt32(Console.ReadLine()!);
+int bz = int.Parse(Console.ReadLine()!);
 
 
 double AB = Math.Sqrt(Math.Pow((bx-ax), 2) + Math.Pow((by-ay), 2) + Math.Pow((bz-az), 2));
